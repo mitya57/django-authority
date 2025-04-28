3ec438fdc7bdb4addf46d5f9cb3dd57f661032a0
Dmitry Shachnev <mitya57@gmail.com>
2025-04-28 12:45:24 +0300

    Replace url with re_path

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,7 @@
 import django.contrib.auth.views
-from django.conf.urls import include, handler500, url
+from django.conf.urls import include, handler500
 from django.conf import settings
+from django.urls import re_path
 
 import authority.views
 import authority.urls
@@ -13,21 +14,21 @@ authority.autodiscover()
 handler500  # flake8
 
 urlpatterns = (
-    url(
+    re_path(
         r"^authority/permission/add/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/$",  # noqa
         view=authority.views.add_permission,
         name="authority-add-permission",
         kwargs={"approved": True, "form_class": SpecialUserPermissionForm},
     ),
-    url(
+    re_path(
         r"^request/add/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/$",  # noqa
         view=authority.views.add_permission,
         name="authority-add-permission-request",
         kwargs={"approved": False, "form_class": SpecialUserPermissionForm},
     ),
-    url(r"^authority/", include(authority.urls)),
-    url(r"^accounts/login/$", django.contrib.auth.views.LoginView.as_view()),
-    url(
+    re_path(r"^authority/", include(authority.urls)),
+    re_path(r"^accounts/login/$", django.contrib.auth.views.LoginView.as_view()),
+    re_path(
         r"^(?P<url>[\/0-9A-Za-z]+)$",
         example.exampleapp.views.top_secret,
         {"lala": "oh yeah!"},
@@ -36,7 +37,7 @@ urlpatterns = (
 
 if settings.DEBUG:
     urlpatterns += (
-        url(
+        re_path(
             r"^media/(?P<path>.*)$",
             django.views.static.serve,
             {"document_root": settings.MEDIA_ROOT,},
